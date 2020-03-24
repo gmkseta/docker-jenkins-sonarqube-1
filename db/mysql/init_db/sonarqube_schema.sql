168dd718911d143d998a2dccf83263b7effb8428
gmkseta <gmkseta@gmail.com>
2020-03-24 16:46:47 +0900

    .env

--- db/mysql/init_db/sonarqube_schema.sql
+++
@@ -1,5 +0,0 @@
-/*
-MySQL - 5.6.30-log : Database - legendshop_docker
-*********************************************************************
-*/
-create database `sonar` default character set utf8 collate utf8_general_ci;
